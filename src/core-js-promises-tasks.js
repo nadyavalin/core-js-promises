29bf781e842cfd68b4240130687fd2c7a02c1ae6
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-04-07 16:58:22 +0300

    feat: solve the function queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -135,8 +135,16 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce(async (acc, current) => {
+    const result = await acc;
+    try {
+      const value = await current;
+      return result + value;
+    } catch {
+      return result;
+    }
+  }, Promise.resolve(''));
 }
 
 module.exports = {
